f4878a6f4d2d7cd31b6eb92ee94be860749c4c59
Moussa Mikhail <moussamikhail56@gmail.com>
2022-03-15 08:33:01 -0500

    refactor and format

--- complex_fraction.py
+++ complex_fraction.py
@@ -7,56 +7,57 @@ from typing import Tuple, Union
 
 
 class ComplexFraction(Complex):
-    """This class implements complex numbers with components being Fractions. 
+    """This class implements complex numbers with components being Fractions.
 
-    In the 1 argument constructor a complex number or a string of a complex number is acceptable.\n
+    In the 1 argument constructor a complex number or a string of a complex number is acceptable.
 
-    The 2 argument constructor accepts 2 real numbers, 2 strings, or a combination.\n
+    The 2 argument constructor accepts 2 real numbers, 2 strings, or a combination.
 
-    Note that 1/3j is interpreted by python as 1/(3j) == -0.333...j\n
+    Note that 1/3j is interpreted by python as 1/(3j) == -0.333...j
     but for the purposes of this class "1/3j" is equivalent to (1/3)j.
-
-
     """
 
-    __slots__ = ('_real', '_imag')
+    __slots__ = ("_real", "_imag")
 
     def __new__(cls, *args) -> ComplexFraction:
 
         self = super(ComplexFraction, cls).__new__(cls)
 
-        if (len(args) == 1):
+        if len(args) == 1:
 
-            z: Union[Complex, str] = args[0]
-            
-            if (isinstance(z, Complex)):
+            z = args[0]
+
+            if isinstance(z, Complex):
 
                 self.from_complex(z)
 
                 return self
-            
-            elif (isinstance(z, str)):
+
+            elif isinstance(z, str):
 
                 self.from_complex_str(z)
 
                 return self
-        
-        elif (len(args) == 2):
+
+            else:
+                raise TypeError(f"Expected {z} to be string or complex")
+
+        elif len(args) == 2:
 
             real: Union[str, Real] = args[0]
 
             imag: Union[str, Real] = args[1]
 
-            self.from_components(real, imag)     
+            self.from_components(real, imag)
 
-            return self    				
+            return self
 
     def from_complex(self, z: Complex):
 
         self._real: Fraction = Fraction(z.real)
 
         self._imag: Fraction = Fraction(z.imag)
-    
+
     @staticmethod
     def split_complex_str(z_str: str) -> Tuple[str, str]:
 
@@ -85,50 +86,50 @@ class ComplexFraction(Complex):
 
         """
 
-        real_str: str = ''
+        real_str: str = ""
 
-        imag_str: str = ''
+        imag_str: str = ""
 
-        if 'j' in z_str:
+        if "j" in z_str:
 
-            curr_pos: int = z_str.find('j') 
+            curr_pos: int = z_str.find("j")
 
             curr_char: str = z_str[curr_pos]
 
-            imag_str = 'j'
+            imag_str = "j"
 
-            while (curr_pos > 0 and curr_char not in {'+', '-'}):
+            while curr_pos > 0 and curr_char not in {"+", "-"}:
 
                 curr_pos -= 1
 
                 curr_char = z_str[curr_pos]
 
                 imag_str = curr_char + imag_str
-        
-            real_str = z_str.replace(imag_str, '')
 
-            real_str = real_str.replace('+', '')
+            real_str = z_str.replace(imag_str, "")
 
-            imag_str = imag_str.replace("*1j", '')
+            real_str = real_str.replace("+", "")
 
-            imag_str = imag_str.replace('j', '')
+            imag_str = imag_str.replace("*1j", "")
 
-            imag_str = imag_str.replace('+', '')
+            imag_str = imag_str.replace("j", "")
 
-            if real_str == '':
+            imag_str = imag_str.replace("+", "")
 
-                real_str = '0'
-        
-        else: 
+            if real_str == "":
+
+                real_str = "0"
+
+        else:
 
             real_str = z_str
 
-            real_str = real_str.replace('+', '')
+            real_str = real_str.replace("+", "")
 
-            imag_str = '0'
+            imag_str = "0"
 
         return real_str, imag_str
-    
+
     def from_complex_str(self, z_str: str):
 
         real_str: str
@@ -138,18 +139,18 @@ class ComplexFraction(Complex):
         real_str, imag_str = ComplexFraction.split_complex_str(z_str)
 
         self.from_components(real_str, imag_str)
-    
+
     def from_components(self, real: Union[str, Real], imag: Union[str, Real]):
-        
+
         self._real: Fraction = Fraction(real)
 
         self._imag: Fraction = Fraction(imag)
-    
+
     @property
     def real(z: ComplexFraction) -> Fraction:
 
         return z._real
-    
+
     @property
     def imag(z: ComplexFraction) -> Fraction:
 
@@ -157,39 +158,39 @@ class ComplexFraction(Complex):
 
     def __repr__(self) -> str:
 
-            return f"ComplexFraction('{self.real}', '{self.imag}')"
-    
+        return f"ComplexFraction('{self.real}', '{self.imag}')"
+
     def __str__(self) -> str:
 
         printable_num: ComplexFraction = self.limit_denominator()
 
-        if (printable_num == 0):
+        if printable_num == 0:
 
-            return '0'
-        
-        elif (printable_num.real == 0):
+            return "0"
+
+        elif printable_num.real == 0:
 
             return f"{printable_num.imag}j"
-        
-        elif (printable_num.imag == 0):
+
+        elif printable_num.imag == 0:
 
             return f"{printable_num.real}"
-        
+
         else:
 
-            if (printable_num.imag > 0):
+            if printable_num.imag > 0:
 
                 return f"{printable_num.real}+{printable_num.imag}j"
-            
+
             else:
 
                 return f"{printable_num.real}-{-printable_num.imag}j"
-            
+
     def __eq__(self, rhs: Number) -> bool:
 
         if isinstance(rhs, Number):
 
-            return self.real == rhs.real and self.imag == rhs.imag    
+            return self.real == rhs.real and self.imag == rhs.imag
 
         elif isinstance(rhs, str):
 
@@ -198,11 +199,11 @@ class ComplexFraction(Complex):
     def __neg__(self) -> ComplexFraction:
 
         return ComplexFraction(-self.real, -self.imag)
-    
+
     def __pos__(self) -> ComplexFraction:
 
         return self
-    
+
     def __add__(self, rhs: Number) -> ComplexFraction:
 
         if isinstance(rhs, Number):
@@ -216,13 +217,13 @@ class ComplexFraction(Complex):
             res_imag = self.imag + rhs.imag
 
             return ComplexFraction(res_real, res_imag)
-        
+
         else:
 
-            return NotImplemented        
-    
+            return NotImplemented
+
     def __radd__(self, lhs) -> ComplexFraction:
-    
+
         if isinstance(lhs, Number):
 
             if not isinstance(lhs, ComplexFraction):
@@ -234,19 +235,19 @@ class ComplexFraction(Complex):
             res_imag = self.imag + lhs.imag
 
             return ComplexFraction(res_real, res_imag)
-        
+
         else:
 
-            return NotImplemented        
-    
+            return NotImplemented
+
     def __sub__(self, rhs) -> ComplexFraction:
 
         return self + (-rhs)
-    
+
     def __rsub__(self, lhs) -> ComplexFraction:
 
         return lhs + (-self)
-    
+
     def __mul__(self, rhs) -> ComplexFraction:
 
         if isinstance(rhs, Number):
@@ -255,16 +256,16 @@ class ComplexFraction(Complex):
 
                 rhs = ComplexFraction(rhs)
 
-            res_real = self.real*rhs.real - self.imag*rhs.imag
+            res_real = self.real * rhs.real - self.imag * rhs.imag
 
-            res_imag = self.real*rhs.imag + self.imag*rhs.real
+            res_imag = self.real * rhs.imag + self.imag * rhs.real
 
             return ComplexFraction(res_real, res_imag)
-        
+
         else:
 
-            return NotImplemented  
-    
+            return NotImplemented
+
     def __rmul__(self, lhs) -> ComplexFraction:
 
         if isinstance(lhs, Number):
@@ -273,12 +274,12 @@ class ComplexFraction(Complex):
 
                 lhs = ComplexFraction(lhs)
 
-            res_real = self.real*lhs.real - self.imag*lhs.imag
+            res_real = self.real * lhs.real - self.imag * lhs.imag
 
-            res_imag = self.real*lhs.imag + self.imag*lhs.real
+            res_imag = self.real * lhs.imag + self.imag * lhs.real
 
             return ComplexFraction(res_real, res_imag)
-        
+
         else:
 
             return NotImplemented
@@ -290,27 +291,27 @@ class ComplexFraction(Complex):
         res_imag = -self.imag
 
         return ComplexFraction(res_real, res_imag)
-    
+
     def magnitude_squared(self) -> Fraction:
 
         return self.real**2 + self.imag**2
-    
+
     def __abs__(self) -> float:
 
         return sqrt(self.magnitude_squared())
-    
-    def recip(self) -> ComplexFraction:
 
-        return self.conjugate()/self.magnitude_squared()
-    
+    def reciprocal(self) -> ComplexFraction:
+
+        return self.conjugate() / self.magnitude_squared()
+
     def __truediv__(self, rhs: Number) -> ComplexFraction:
 
-        return self*(1/rhs)
-    
+        return self * (1 / rhs)
+
     def __rtruediv__(self, lhs: Number) -> ComplexFraction:
 
-        return lhs*self.recip()
-    
+        return lhs * self.reciprocal()
+
     def limit_denominator(self, max_denominator=1000000) -> ComplexFraction:
 
         new_real: Fraction = self.real.limit_denominator(max_denominator)
@@ -318,7 +319,7 @@ class ComplexFraction(Complex):
         new_imag: Fraction = self.imag.limit_denominator(max_denominator)
 
         return ComplexFraction(new_real, new_imag)
-    
+
     def __complex__(self) -> Complex:
 
         real: float = float(self.real)
@@ -326,27 +327,24 @@ class ComplexFraction(Complex):
         imag: float = float(self.imag)
 
         return complex(real, imag)
-    
+
     def __pow__(self, power: Complex) -> ComplexFraction:
 
-        if (isinstance(power, int)):
-
-            res: ComplexFraction = ComplexFraction(1, 0)
-
-            for _ in range(abs(power)):
-
-                res = res * self
-            
-            if (power < 0):
-
-                res = 1/res
-            
-            return res
-        
-        else:
-
+        if not (isinstance(power, int)):
             return ComplexFraction(complex(self) ** power)
-        
+
+        res: ComplexFraction = ComplexFraction(1, 0)
+
+        for _ in range(abs(power)):
+
+            res = res * self
+
+        if power < 0:
+
+            res = 1 / res
+
+        return res
+
     def __rpow__(self, base: Complex) -> ComplexFraction:
 
         return ComplexFraction(base ** complex(self))
--- testing/test_complex_fraction.py
+++ testing/test_complex_fraction.py
@@ -4,136 +4,174 @@ from complex_fraction import ComplexFraction
 
 from fractions import Fraction
 
+
 @pytest.fixture
 def real_num() -> ComplexFraction:
 
-    return ComplexFraction('2.5', "0")
+    return ComplexFraction("2.5", "0")
+
 
 @pytest.fixture
 def imag_num() -> ComplexFraction:
 
-    return ComplexFraction("0", '-1/2')
+    return ComplexFraction("0", "-1/2")
+
 
 @pytest.fixture
 def complex_num() -> ComplexFraction:
 
-    return ComplexFraction('1/3', '-1/4')
+    return ComplexFraction("1/3", "-1/4")
 
 
 def test_init(complex_num):
 
-    assert complex_num.real == Fraction('1/3')
+    assert complex_num.real == Fraction("1/3")
 
-    assert complex_num.imag == Fraction('-1/4')
+    assert complex_num.imag == Fraction("-1/4")
+
 
 def test_repr_real(real_num):
 
     assert real_num.__repr__() == "ComplexFraction('5/2', '0')"
 
+
 def test_repr_imag(imag_num):
 
     assert imag_num.__repr__() == "ComplexFraction('0', '-1/2')"
+
 
 def test_repr(complex_num):
 
     assert complex_num.__repr__() == "ComplexFraction('1/3', '-1/4')"
 
+
 def test_str_real(real_num):
 
     assert real_num.__str__() == "5/2"
+
 
 def test_str_imag(imag_num):
 
     assert imag_num.__str__() == "-1/2j"
 
+
 def test_str(complex_num):
 
     assert complex_num.__str__() == "1/3-1/4j"
 
+
 def test_eq(complex_num):
 
-    assert complex_num == ComplexFraction('1/3', '-1/4')
+    assert complex_num == ComplexFraction("1/3", "-1/4")
+
 
 def test_neg(complex_num):
 
-    assert -complex_num == ComplexFraction('-1/3', '1/4')
+    assert -complex_num == ComplexFraction("-1/3", "1/4")
+
 
 def test_add(real_num, imag_num):
 
-    assert real_num + imag_num == ComplexFraction('5/2', '-1/2')
+    assert real_num + imag_num == ComplexFraction("5/2", "-1/2")
+
 
 def test_sub(complex_num, real_num):
 
-    assert complex_num - real_num == ComplexFraction('1/3', '-1/4') - ComplexFraction('5/2', '0')
+    assert complex_num - real_num == ComplexFraction("1/3", "-1/4") - ComplexFraction(
+        "5/2", "0"
+    )
+
 
 def test_mul(complex_num):
 
-    assert complex_num * ComplexFraction(1,1) == ComplexFraction('1/3', '-1/4') + ComplexFraction('1/4', '1/3')
+    assert complex_num * ComplexFraction(1, 1) == ComplexFraction(
+        "1/3", "-1/4"
+    ) + ComplexFraction("1/4", "1/3")
+
 
 def test_truediv(complex_num, imag_num):
 
-    assert complex_num/imag_num == ComplexFraction('1/2', '2/3')
+    assert complex_num / imag_num == ComplexFraction("1/2", "2/3")
+
 
 def test_conjugate(complex_num):
 
-    assert complex_num.conjugate() == ComplexFraction('1/3', '1/4')
+    assert complex_num.conjugate() == ComplexFraction("1/3", "1/4")
+
 
 def test_mag_sq(complex_num):
 
-    assert complex_num.magnitude_squared() == Fraction('1/9') + Fraction('1/16')
+    assert complex_num.magnitude_squared() == Fraction("1/9") + Fraction("1/16")
 
-def test_recip(complex_num):
 
-    assert complex_num.recip() == ComplexFraction('1/3', '1/4')/(Fraction('1/9') + Fraction('1/16'))
+def test_reciprocal(complex_num):
+
+    assert complex_num.reciprocal() == ComplexFraction("1/3", "1/4") / (
+        Fraction("1/9") + Fraction("1/16")
+    )
+
 
 def test_pow_pos_int(complex_num):
 
-    assert complex_num**2 == complex_num*complex_num
+    assert complex_num**2 == complex_num * complex_num
+
 
 def test_pow_neg_int(complex_num):
 
-    assert complex_num**-2 == 1/(complex_num*complex_num)
+    assert complex_num**-2 == 1 / (complex_num * complex_num)
+
 
 ###  split_complex_str tests  ###
+
 
 def test_real_str_1():
 
     assert ComplexFraction.split_complex_str("1.1") == ("1.1", "0")
 
+
 def test_real_str_2():
 
     assert ComplexFraction.split_complex_str("-1/3") == ("-1/3", "0")
+
 
 def test_imag_str_1():
 
     assert ComplexFraction.split_complex_str("1.1j") == ("0", "1.1")
 
+
 def test_imag_str_2():
 
     assert ComplexFraction.split_complex_str("-11/10j") == ("0", "-11/10")
+
 
 def test_complex_str_1():
 
     assert ComplexFraction.split_complex_str("1+1j") == ("1", "1")
 
+
 def test_complex_str_2():
 
     assert ComplexFraction.split_complex_str("1/3+1j") == ("1/3", "1")
+
 
 def test_complex_str_3():
 
     assert ComplexFraction.split_complex_str("-1/3+1j") == ("-1/3", "1")
 
+
 def test_complex_str_4():
 
     assert ComplexFraction.split_complex_str("-1.1-2.1j") == ("-1.1", "-2.1")
+
 
 def test_complex_str_5():
 
     assert ComplexFraction.split_complex_str("-1.1j-2.1") == ("-2.1", "-1.1")
 
+
 def test_complex_str_6():
 
     assert ComplexFraction.split_complex_str("-11/10j+2.1") == ("2.1", "-11/10")
+
 
 ###  end split_complex_str tests  ###
